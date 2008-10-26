a0d62d6bbfcb24ed412ae4212ba777443bba7877
yorkultimate_david <yorkultimate_david@d6e9c351-0237-4953-a62f-a7a1ee97849f>
2008-10-26 09:25:20 +0000

    git-svn-id: https://svn2.assembla.com/svn/yorkultimate@7 d6e9c351-0237-4953-a62f-a7a1ee97849f

--- htdocs/news.php
+++ htdocs/news.php
@@ -1,5 +1,5 @@
 <p>
-	<b>3-legged bar crawl.</b> Wednesday. 7pm. Derwent. Legendary.
+	Training has begun and it's been great to see so many quality new players. The Loughborough beginners tournament was successful and we're looking forward to the indoor regionals.
 </p>
 <p>
 	<a class="internal link" href="calendar.php#calendar">Calendar</a>
