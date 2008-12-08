26004b0db717e66de99b6177d3a4630ade8bf4db
yorkultimate_david <yorkultimate_david@d6e9c351-0237-4953-a62f-a7a1ee97849f>
2008-12-08 12:47:51 +0000

    news from winter league
    git-svn-id: https://svn2.assembla.com/svn/yorkultimate@12 d6e9c351-0237-4953-a62f-a7a1ee97849f

--- htdocs/news.php
+++ htdocs/news.php
@@ -1,5 +1,5 @@
 <p>
-	Great weekend for the firsts, who won the plate, and the seconds who were one of the top spirited sides!
+	We schooled the Manor boys and ran out of time in an astonishing comeback against York's finest non-uni players at winter league two.
 </p>
 <p>
 	<a class="internal link" href="calendar.php#calendar">Calendar</a>
