9e257bfb335e840ec6fd14c3c3a944a732fb2cae
yorkultimate_david <yorkultimate_david@d6e9c351-0237-4953-a62f-a7a1ee97849f>
2008-12-19 11:03:25 +0000

    happy christmas
    git-svn-id: https://svn2.assembla.com/svn/yorkultimate@15 d6e9c351-0237-4953-a62f-a7a1ee97849f

--- htdocs/news.php
+++ htdocs/news.php
@@ -1,5 +1,5 @@
 <p>
-	We schooled the Manor boys and ran out of time in an astonishing comeback against York's finest non-uni players at winter league two.
+	HAPPY CHRISTMAS EVERYONE!!
 </p>
 <p>
 	<a class="internal link" href="calendar.php#calendar">Calendar</a>
