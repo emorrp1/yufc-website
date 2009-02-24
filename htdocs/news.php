e07f7a54c9183e7c51bab2acf45dc3ba24e74768
yorkultimate_chetan <yorkultimate_chetan@d6e9c351-0237-4953-a62f-a7a1ee97849f>
2009-02-24 17:19:19 +0000

    beginners training news
    git-svn-id: https://svn2.assembla.com/svn/yorkultimate@25 d6e9c351-0237-4953-a62f-a7a1ee97849f

--- htdocs/news.php
+++ htdocs/news.php
@@ -1,5 +1,5 @@
 <p>
-	Roses is coming in week 2, and this year it's in York!
+	This week is <b>beginners week</b>! <i>Anybody is welcome</i> to come to any of our training sessions and give Ultimate a go. Don't worry, we'll show you the ropes!
 </p>
 <p>
 	<a class="internal link" href="calendar.php#calendar">Calendar</a>
