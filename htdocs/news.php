67e8745ca2bc871d3c197ec4234adc832bf44552
yorkultimate_chetan <yorkultimate_chetan@d6e9c351-0237-4953-a62f-a7a1ee97849f>
2009-05-09 23:13:33 +0000

    news: roses results
    git-svn-id: https://svn2.assembla.com/svn/yorkultimate@35 d6e9c351-0237-4953-a62f-a7a1ee97849f

--- htdocs/news.php
+++ htdocs/news.php
@@ -1,6 +1,7 @@
 <p>
-	Come and support us at <b>Roses</b> this Friday at 3:30pm on 22 Acres when we will be taking on Lancaster!
-	Facebook group <a href="http://www.facebook.com/event.php?eid=72494793540">here</a>.
+	Well done to all that played Roses! We were beaten indoors but we came back to put in a MASSIVE performance outdoors. What a way to finish the season!
+	<br />
+	<a class="external link" href="http://www.roses2009.com">Click here for live Roses updates.</a>
 </p>
 <p>
 	<a class="internal link" href="calendar.php#calendar">Calendar</a>
