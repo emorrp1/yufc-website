2743a00212d03e5fe5d5dda1ccb36f2a2a9f34c7
Philip Morrell <yufc.emorrp1@mamber.net>
2010-05-13 10:38:39 +0100

    news, copyright

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 			<div id="footer">
 				<p class="comment">Last updated: <?php include('lastupdated.txt') ?></p>
 				<p>
-				    &copy; 2008 - 2009 University of York Ultimate Club - All rights reserved
+				    &copy; 2008 - 2010 University of York Ultimate Club - All rights reserved
 				</p>
 				<p>
     				<a target="_blank" href="http://validator.w3.org/check?uri=referer"><img src="images/w3c/valid_xhtml.gif" alt="XHTML Valid"/></a>
--- news.php
+++ news.php
@@ -1,3 +1,6 @@
 <p>
 	York Ultimate excelled this Spring, reaching 6th in University Mixed Indoor Nationals in Glasgow, whilst winning the Spirit of the Game award and entering many other tournaments, both serious and fun!
 </p>
+<p>
+	Thanks to an excellent elections evening, we now have a new <a href="about.html#committee">committee</a>, with all new captains to run training sessions.
+</p>
