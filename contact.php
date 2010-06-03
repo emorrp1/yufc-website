d94a8a256d09783d63acbfaeb769cb5b2a883737
Philip Morrell <ubuntu.emorrp1@mamber.net>
2010-06-03 01:21:52 +0100

    updating mailing list contacts

--- contact.php
+++ contact.php
@@ -6,23 +6,19 @@
 
 <div class="shortcuts">
 	<ul>
-		<li><a href="#yahoo">Yahoo Sports Group</a></li>
+		<li><a href="#list">Mailing List</a></li>
 		<li><a href="#facebook">Facebook</a></li>
 		<li><a href="#email">Email</a></li>
 	</ul>
 </div>
 
-<h3 id="yahoo">Yahoo Sports Group</h3>
+<h3 id="list">Mailing List</h3>
 <table style="margin-left:2.0em;width:90%;"><tbody><tr>
 	<td width="33%">
-		<a target="_blank" href="http://sports.groups.yahoo.com/group/yorkultimate/"><img alt="York Ultimate on Yahoo Groups" src="images/contact/yahoo.png"/></a>
+		<p><a class="external link" target="_blank" href="https://lists.york.ac.uk/mailman/listinfo/ultimate-club">Visit the mailing list</a></p>
 	</td>
 	<td width="66%">
-		<p>This our main way of staying in touch. Go to the <a class="external link" target="_blank" href="http://sports.groups.yahoo.com/group/yorkultimate/">Yahoo Sports Group</a> to sign up (or use the form below) and <i>change the settings so you get the messages via email</i>.</p>
-		<form method="get" action="http://groups.yahoo.com/subscribe/yorkultimate">
-			<input type="text" name="user" value="enter email address" size="15" />
-			<input type="submit" value="Subscribe" />
-		</form>
+		<p>This our main way of staying in touch. Go to the <a class="external link" target="_blank" href="https://lists.york.ac.uk/mailman/listinfo/ultimate-club">York Ultimate Mailing List</a> to sign up.</p>
 	</td>		
 </tr></tbody></table>
 
--- news.php
+++ news.php
@@ -4,3 +4,6 @@
 <p>
 	Thanks to an excellent elections evening, we now have a new <a href="about.html#committee">committee</a>, with all new captains to run training sessions.
 </p>
+<p>
+	We're moving the Club mailing list from Yahoo Groups to York Lists, to make it easier for our freshers to manage. You can sign yourselves up <a href="https://lists.york.ac.uk/mailman/listinfo/ultimate-club">here</a>.
+</p>
