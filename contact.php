c5d2f55ea4549fabd986efb131e072c9227d549c
Philip Morrell <ubuntu.emorrp1@mamber.net>
2010-06-03 01:31:09 +0100

    nicer table formatting

--- contact.php
+++ contact.php
@@ -25,7 +25,7 @@
 <h3 id="facebook">Facebook</h3>
 <table style="margin-left:2.0em;width:90%;"><tbody><tr>
 	<td width="33%">
-		<a target="_blank" href="http://www.facebook.com/group.php?gid=2203901067"><img alt="York Ultimate on Facebook" src="images/contact/facebook.png"/></a>
+		<p><a target="_blank" href="http://www.facebook.com/group.php?gid=2203901067"><img alt="York Ultimate on Facebook" src="images/contact/facebook.png"/></a></p>
 	</td>
 	<td width="66%">
 		<p>Join our <a class="external link" target="_blank" href="http://www.facebook.com/group.php?gid=2203901067">Facebook Group</a></p>
@@ -33,7 +33,11 @@
 </tr></tbody></table>
 
 <h3 id="email">Email</h3>
-<p><a class="email link" href="mailto:info@yorkultimate.co.uk">Click here to email us</a></p>
+<table style="margin-left:2.0em;width:90%;"><tbody><tr>
+	<td width="33%">
+		<p><a class="email link" href="mailto:info@yorkultimate.co.uk">Click here to email us</a></p>
+	</td>
+</tr></tbody></table>
 
 <?php
 	include('footer.php');
