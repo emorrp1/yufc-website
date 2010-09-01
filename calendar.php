8b68dc806f070448bcacc3d5588ba7d30063876e
Philip Morrell <ubuntu.emorrp1@mamber.net>
2010-09-01 23:34:58 +0100

    updating calendar with training schedule

--- calendar.php
+++ calendar.php
@@ -7,8 +7,8 @@
 <div class="shortcuts">
 	<ul>
 		<li><a href="#training">Training</a></li>
-<!--
 		<li><a href="#calendar">Upcoming events</a></li>
+<!--
 		<li><a href="#feeds">Calendar feeds</a></li>
 -->
 	</ul>
@@ -25,9 +25,10 @@
 		<li><b>Sunday 7:30</b> : Indoors @ Sports Centre Tent</li>
 </ul>
 
-<!--
 <h3 id="calendar">Upcoming events</h3>
 <p>Here you can see what we're up to in the near future including <b>socials</b>, <b>training</b> and <b>tournaments</b>.</p>
+<p><a class="link" href="documents/Autumn_Training.xls">Autumn Training (xls)</a></p>
+<!--
 <p style="text-align:center;">
 	<!-- following provided by Google Calendar : http://www.google.com/calendar/embedhelper -->
 	<iframe src="http://www.google.com/calendar/embed?title=York%20Ultimate&amp;showTitle=0&amp;showDate=0&amp;showCalendars=0&amp;mode=AGENDA&amp;height=400&amp;wkst=2&amp;bgcolor=%23000000&amp;src=qhvtkilcl3gvtlfvhi2id2oveo%40group.calendar.google.com&amp;color=%23A32929&amp;src=or4qdk6tsdqr6hcmlprufhjjvg%40group.calendar.google.com&amp;color=%234E5D6C&amp;ctz=Europe%2FLondon" style="border:none;" width="90%" height="400" frameborder="0" scrolling="no"></iframe>
